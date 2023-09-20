1011d9ae87fd9a55ee99f4c3dc4938568773b73e
AlexItalbazar <a-egorov@bk.ru>
2023-09-20 17:06:58 +0300

    init

---
+++ unique.py
@@ -0,0 +1,55 @@
+import sys
+from time import time
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication
+from gui import Ui_MainWindow
+from unique_func import CounterUniqueWords
+
+
+class UniqueWords(QMainWindow, Ui_MainWindow, QFileDialog):
+    def __init__(self):
+        super(UniqueWords, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.setupUi(self)
+        self.setWindowTitle('Alex words counter')
+        self.setWindowIcon(QIcon('al.ico'))
+        self.pushButton.clicked.connect(self.start)
+        self.pushButton_clear.clicked.connect(self.lcd_clear)
+        self.pushButton_safe.clicked.connect(self.safe_dict)
+        self._ = CounterUniqueWords(self)
+        self.counter_obj = CounterUniqueWords(self)
+
+    def lcd_clear(self):
+        self.lcdNumber.display(0)
+        self.progressBar.setValue(0)
+        self.label.setText('Счетчик уникальных слов:')
+
+    def safe_dict(self):
+        path_for_safe = QFileDialog.getSaveFileName(self, "Сохраняем словарь", filter="текстовый файл (*.txt)")[0]
+        self.counter_obj.safe_unique_words(path_for_safe)
+
+    def start(self):
+        self.lcd_clear()
+        file_path = \
+            QFileDialog.getOpenFileName(self, "Выбери текстовый файл", "/home/",
+                                        filter="текстовый файл (*.txt)")[0]
+
+        if self.checkBox.isChecked():
+            flag_normal_form = True
+        else:
+            flag_normal_form = False
+        min_symbols = self.spinBox.value()
+        start_time = time()
+        if file_path:
+            unique_words_num = self.counter_obj.different_words_func(file_path, flag_normal_form, min_symbols)
+            self.lcdNumber.display(unique_words_num)
+            self.label.setText(f'Счетчик уникальных слов: посчитали за {round(time() - start_time, 2)} сек')
+            self.progressBar.setValue(100)
+            self.pushButton_safe.setEnabled(True)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = UniqueWords()
+    w.show()
+    sys.exit(app.exec_())
---
+++ unique_func.py
@@ -0,0 +1,45 @@
+import math
+import os
+import re
+import pymorphy3
+
+
+class CounterUniqueWords:
+    def __init__(self, main_window):
+        self.morph = pymorphy3.MorphAnalyzer(path='pymorphy3_dicts_ru')
+        self.main_window = main_window
+        self.pattern = re.compile(r'[а-яёa-z]+')
+        self.progressBar = main_window.progressBar
+        self.label = main_window.label
+        self.unique_words = set()
+
+    def file_reader(self, path):
+        path = os.path.normpath(path)
+        with open(path, 'r', encoding='utf-8') as f:
+            self.label.setText('Счетчик уникальных слов: читаем файл..')
+            return f.read().lower()
+
+    def safe_unique_words(self, path_for_safe):
+        if path_for_safe:
+            path = os.path.normpath(path_for_safe)
+            with open(path, 'w', encoding='utf-8') as f:
+                f.write('\n'.join(self.unique_words))
+
+    def different_words_func(self, file_path, flag_normal_form, min_symbols):
+        self.unique_words.clear()
+        string_text = self.file_reader(file_path)
+        list_words = string_text.split()
+        max_count = len(list_words)
+        for count, word in enumerate(list_words):
+            # x = 100 * count / max_count
+            self.progressBar.setValue(math.ceil(100 * count / max_count))
+            filtered_word = self.pattern.search(word)
+            self.label.setText('Счетчик уникальных слов: фильтруем')
+            word = filtered_word[0] if filtered_word else ''
+            if word and len(word) >= min_symbols:
+                if flag_normal_form:
+                    word = self.morph.parse(word)[0].normal_form
+                self.unique_words.add(word)
+                self.label.setText('Счетчик уникальных слов: считаем')
+
+        return len(self.unique_words)
