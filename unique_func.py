44976b3e9fd86e9a17de7d166e9a7df1008962ae
AlexPloy <alexey.himki@gmail.com>
2023-09-26 20:28:16 +0300

    исправил ошибку

--- unique_func.py
+++ unique_func.py
@@ -32,25 +32,6 @@ class CounterUniqueWords:
             with open(path, 'w', encoding='utf-8') as f:
                 f.write('\n'.join(self.unique_words))
 
-    # def different_words_func(self, file_path, flag_normal_form, min_symbols):
-    #     if self.checkBox_rus.isChecked():
-    #         pattern = self.pattern_ru
-    #     else:
-    #         pattern = self.pattern
-    #     self.unique_words.clear()
-    #     string_text = self.file_reader(file_path)
-    #     string_text = self.pattern_punctuation.sub(' ', string_text)
-    #     list_words = string_text.split()
-    #     max_count = len(list_words)
-    #     for count, word in enumerate(list_words):
-    #         self.progressBar.setValue(math.ceil(100 * count / max_count))
-    #         self.label.setText('Счетчик уникальных слов: фильтруем')
-    #         if (filtered_word := pattern.search(word)[0]) and len(filtered_word) >= min_symbols:
-    #             if flag_normal_form:
-    #                 filtered_word = self.morph.parse(filtered_word)[0].normal_form
-    #             self.label.setText('Счетчик уникальных слов: считаем')
-    #             self.unique_words.add(filtered_word)
-    #     return len(self.unique_words)
 
     def different_words_func(self, file_path, flag_normal_form, min_symbols):
         if self.checkBox_rus.isChecked():
